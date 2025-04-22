44881a682149826c1d164ad6386e4ca3f7edfeca
Md Faizan <144156089+faizanwebd@users.noreply.github.com>
2025-04-22 14:40:17 +0530

    Time: 0 ms (100%), Space: 14.6 MB (60.02%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<bool> visited(n, false);
+        queue<int> q;
+        q.push(0);
+        visited[0] = true;
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            for (int key : rooms[curr]) {
+                if (!visited[key]) {
+                    visited[key] = true;
+                    q.push(key);
+                }
+            }
+        }
+
+        for (bool v : visited) {
+            if (!v) return false;
+        }
+        return true;
+    }
+};
