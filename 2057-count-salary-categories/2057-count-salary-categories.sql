1f8421bb1658509a8b68b55dcafe0e19ab789578
Md Faizan <144156089+faizanwebd@users.noreply.github.com>
2025-05-01 14:17:59 +0530

    Time: 1511 ms (79.71%), Space: 0B (100%) - LeetHub

---
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -0,0 +1,19 @@
+SELECT c.category, 
+       COUNT(a.category) AS accounts_count
+FROM (
+    SELECT 'Low Salary' AS category
+    UNION ALL
+    SELECT 'Average Salary'
+    UNION ALL
+    SELECT 'High Salary'
+) AS c
+LEFT JOIN (
+    SELECT CASE
+             WHEN income < 20000 THEN 'Low Salary'
+             WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+             ELSE 'High Salary'
+           END AS category
+    FROM Accounts
+) AS a
+ON c.category = a.category
+GROUP BY c.category;
