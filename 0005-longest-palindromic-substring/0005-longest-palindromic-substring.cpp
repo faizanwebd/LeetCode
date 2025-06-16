2be8fc2ff561bce27f45a8f7c55e4fad46801c56
Md Faizan <144156089+faizanwebd@users.noreply.github.com>
2025-06-16 16:22:01 +0530

    Time: 12 ms (65.94%), Space: 9.4 MB (82.99%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        // if (s.empty()) return "";
+        if (s.empty()) return "";
         int start = 0, maxLen = 1;
 
         for (int i = 0; i < s.length(); ++i) {
