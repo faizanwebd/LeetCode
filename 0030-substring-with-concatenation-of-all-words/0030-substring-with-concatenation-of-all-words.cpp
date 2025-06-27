4fed261da207102360be0257c7ccb051eed5690b
Md Faizan <144156089+faizanwebd@users.noreply.github.com>
2025-06-27 14:49:27 +0530

    Time: 26 ms (49.65%), Space: 23.8 MB (84.64%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> result;
+        if (words.empty()) return result;
+        int wordLen = words[0].size();
+        int wordCount = words.size();
+        int totalLen = wordLen * wordCount;
+        if (s.size() < totalLen) return result;
+
+        unordered_map<string, int> wordFreq;
+        for (const string& word : words) wordFreq[word]++;
+
+        for (int i = 0; i < wordLen; ++i) {
+            int left = i, count = 0;
+            unordered_map<string, int> window;
+            for (int j = i; j + wordLen <= s.size(); j += wordLen) {
+                string word = s.substr(j, wordLen);
+                if (wordFreq.count(word)) {
+                    window[word]++;
+                    count++;
+                    while (window[word] > wordFreq[word]) {
+                        string leftWord = s.substr(left, wordLen);
+                        window[leftWord]--;
+                        count--;
+                        left += wordLen;
+                    }
+                    if (count == wordCount) result.push_back(left);
+                } else {
+                    window.clear();
+                    count = 0;
+                    left = j + wordLen;
+                }
+            }
+        }
+        return result;
+    }
+};
