3566e318028228cb0ea5254904627f8967c9d586
Md Faizan <144156089+faizanwebd@users.noreply.github.com>
2025-07-29 23:12:42 +0530

    Time: 39 ms (39.66%), Space: 60.6 MB (71.12%) - LeetHub

--- 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -3,7 +3,7 @@ class Solution {
   vector<int> smallestSubarrays(vector<int>& nums) {
     constexpr int kMaxBit = 30;
     vector<int> ans(nums.size(), 1);
-    // closest[j] := the closest index i s.t. the j-th bit of nums[i] is 1
+
     vector<int> closest(kMaxBit);
 
     for (int i = nums.size() - 1; i >= 0; --i)
